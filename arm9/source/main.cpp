3921f6db9a6bbbcb1766510537f21fe41ac43990
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-02 09:42:10 -0700

    Update main.cpp

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -19,10 +19,9 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <limits.h>
-
 #include <string.h>
 #include <unistd.h>
-
+#include "common/file_browse.h"
 #include "common/nds_loader_arm9.h"
 //---------------------------------------------------------------------------------
 int main(int argc, char **argv) {
@@ -30,12 +29,14 @@ int main(int argc, char **argv) {
 
 	videoSetModeSub(MODE_0_2D);
 	vramSetBankH(VRAM_H_SUB_BG);
-	consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 15, 0, false, true);
-
+	consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x192, 15, 0, false, true);
 	sysSetCardOwner (BUS_OWNER_ARM9);
 
-  	fatInitDefault();
-	{
+	if (!fatInitDefault()) {
+		iprintf ("fatinitDefault failed!\n");
+		stop();
+	}
+
   scanKeys();
 	int pressed = keysHeld();
 
