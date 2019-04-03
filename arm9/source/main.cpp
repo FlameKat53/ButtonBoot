5d757bd775caed2540a9d0ca0b933db0919af531
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-03 12:15:41 -0700

    Add reading .nds file path from an ini file
    * inifile.cpp
    
    * inifile.h
    
    * \n some stuff and include inifile.h
    
    * Update main.cpp
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * disable stuff which is unneeded for just building
    
    * Update README.md
    
    * Create stringtool.h
    
    * Create stringtool.cpp
    
    * Update .travis.yml
    
    * silly travis CI
    
    * clean code a tiny bit
    
    * Update io_dldi.h
    
    * Update main.cpp
    
    * move stuffs around for better use
    
    * Update main.cpp
    
    * Update main.cpp
    
    * .c_str()
    
    * Update main.cpp
    
    * ini base
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Update main.cpp
    
    * oops
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Simplify a bit
    
    * Move the `CIniFile ini`... below fatInitDefault()
    
    and fix some indentation

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <fat.h>
 #include "common/nds_loader_arm9.h"
+#include "inifile.h"
 //---------------------------------------------------------------------------------
 void stop (void) {
 //---------------------------------------------------------------------------------
@@ -34,6 +35,18 @@ void stop (void) {
 //---------------------------------------------------------------------------------
 int main(int argc, char **argv) {
 //---------------------------------------------------------------------------------
+	std::string bootA = "/_nds/extras/bootA.nds";
+	std::string bootB = "/_nds/extras/bootB.nds";
+	std::string bootX = "/_nds/extras/bootX.nds";
+	std::string bootY = "/_nds/extras/bootY.nds";
+	std::string bootL = "/_nds/extras/bootL.nds";
+	std::string bootR = "/_nds/extras/bootR.nds";
+	std::string bootDown = "/_nds/extras/bootDown.nds";
+	std::string bootUp = "/_nds/extras/bootUp.nds";
+	std::string bootLeft = "/_nds/extras/bootLeft.nds";
+	std::string bootRight = "/_nds/extras/bootRight.nds";
+	std::string bootSelect = "/_nds/extras/bootSelect.nds";
+	std::string bootDefault = "/boot.nds";
 
 	videoSetModeSub(MODE_0_2D);
 	vramSetBankH(VRAM_H_SUB_BG);
@@ -43,92 +56,124 @@ int main(int argc, char **argv) {
 		iprintf ("fatInitDefault failed!\n");
 		stop();
 	}
+	CIniFile ini("/_nds/extras/ButtonBoot.ini");
+	
+	bootA = ini.GetString("BUTTONBOOT", "BOOT-A_PATH", bootA);
+	bootB = ini.GetString("BUTTONBOOT", "BOOT-B_PATH", bootB);
+	bootX = ini.GetString("BUTTONBOOT", "BOOT-X_PATH", bootX);
+	bootY = ini.GetString("BUTTONBOOT", "BOOT-Y_PATH", bootY);
+	bootL = ini.GetString("BUTTONBOOT", "BOOT-L_PATH", bootL);
+	bootR = ini.GetString("BUTTONBOOT", "BOOT-R_PATH", bootR);
+	bootDown = ini.GetString("BUTTONBOOT", "BOOT-DOWN_PATH", bootDown);
+	bootUp = ini.GetString("BUTTONBOOT", "BOOT-UP_PATH", bootUp);
+	bootLeft = ini.GetString("BUTTONBOOT", "BOOT-LEFT_PATH", bootLeft);
+	bootRight = ini.GetString("BUTTONBOOT", "BOOT-RIGHT_PATH", bootRight);
+	bootSelect = ini.GetString("BUTTONBOOT", "BOOT-SELECT_PATH", bootSelect);
+	bootDefault = ini.GetString("BUTTONBOOT", "BOOT-DEFAULT_PATH", bootDefault);
+
+	ini.SetString("BUTTONBOOT", "BOOT-A_PATH", bootA);
+	ini.SetString("BUTTONBOOT", "BOOT-B_PATH", bootB);
+	ini.SetString("BUTTONBOOT", "BOOT-X_PATH", bootX);
+	ini.SetString("BUTTONBOOT", "BOOT-Y_PATH", bootY);
+	ini.SetString("BUTTONBOOT", "BOOT-L_PATH", bootL);
+	ini.SetString("BUTTONBOOT", "BOOT-R_PATH", bootR);
+	ini.SetString("BUTTONBOOT", "BOOT-DOWN_PATH", bootDown);
+	ini.SetString("BUTTONBOOT", "BOOT-UP_PATH", bootUp);
+	ini.SetString("BUTTONBOOT", "BOOT-LEFT_PATH", bootLeft);
+	ini.SetString("BUTTONBOOT", "BOOT-RIGHT_PATH", bootRight);
+	ini.SetString("BUTTONBOOT", "BOOT-SELECT_PATH", bootSelect);
+	ini.SetString("BUTTONBOOT", "BOOT-DEFAULT_PATH", bootDefault);
+
+	mkdir("/_nds/",0777);
+	mkdir("/_nds/extras/",0777);
+	ini.SaveIniFile("/_nds/extras/ButtonBoot.ini");
+
 
   scanKeys();
 	int pressed = keysHeld();
 
 	if (pressed & KEY_A) {
-		if((access("/_nds/extras/bootA.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootA.nds", 0, NULL, false);
+		if((access(bootA.c_str(), F_OK) == 0)) {
+			runNdsFile(bootA.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootA.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootA.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_B) {
-		if((access("/_nds/extras/bootB.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootB.nds", 0, NULL, false);
+		if((access(bootB.c_str(), F_OK) == 0)) {
+			runNdsFile(bootB.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootB.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootB.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_X) {
-		if((access("/_nds/extras/bootX.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootX.nds", 0, NULL, false);
+		if((access(bootX.c_str(), F_OK) == 0)) {
+			runNdsFile(bootX.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootX.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootX.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_Y) {
-		if((access("/_nds/extras/bootY.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootY.nds", 0, NULL, false);
+		if((access(bootY.c_str(), F_OK) == 0)) {
+			runNdsFile(bootY.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootY.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootY.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_L) {
-		if((access("/_nds/extras/bootL.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootL.nds", 0, NULL, false);
+		if((access(bootL.c_str(), F_OK) == 0)) {
+			runNdsFile(bootL.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootL.nds wasn't found!");
+			printf("Error:\n %s wasn't found!", bootL.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_R) {
-		if((access("/_nds/extras/bootR.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootR.nds", 0, NULL, false);
+		if((access(bootR.c_str(), F_OK) == 0)) {
+			runNdsFile(bootR.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootR.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootR.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_RIGHT) {
-		if((access("/_nds/extras/bootRight.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootRight.nds", 0, NULL, false);
+		if((access(bootRight.c_str(), F_OK) == 0)) {
+			runNdsFile(bootRight.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootRight.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootRight.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_LEFT) {
-		if((access("/_nds/extras/bootLeft.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootLeft.nds", 0, NULL, false);
+		if((access(bootLeft.c_str(), F_OK) == 0)) {
+			runNdsFile(bootLeft.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootLeft.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootLeft.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_DOWN) {
-		if((access("/_nds/extras/bootDown.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootDown.nds", 0, NULL, false);
+		if((access(bootDown.c_str(), F_OK) == 0)) {
+			runNdsFile(bootDown.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootDown.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootDown.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_UP) {
-		if((access("/_nds/extras/bootUp.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootUp.nds", 0, NULL, false);
+		if((access(bootUp.c_str(), F_OK) == 0)) {
+			runNdsFile(bootUp.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootUp.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootUp.c_str());
 			stop();
 		}
 	} else if (pressed & KEY_SELECT) {
-		if((access("/_nds/extras/bootSelect.nds", F_OK) == 0)) {
-			runNdsFile("/_nds/extras/bootSelect.nds", 0, NULL, false);
+		if((access(bootSelect.c_str(), F_OK) == 0)) {
+			runNdsFile(bootSelect.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: bootSelect.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootSelect.c_str());
 			stop();
 		}
 	} else {
-		if((access("/boot.nds", F_OK) == 0)) {
-			runNdsFile("/boot.nds", 0, NULL, false);
+		if((access(bootDefault.c_str(), F_OK) == 0)) {
+			runNdsFile(bootDefault.c_str(), 0, NULL, false);
 		} else {
-			printf("Error: boot.nds wasn't found!");
+			printf("Error:\n%s wasn't found!", bootDefault.c_str());
 			stop();
 		}
 	}
