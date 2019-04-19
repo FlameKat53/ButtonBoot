efc4a13879b2fbade103abd247db4be249572621
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-19 10:04:24 -0700

    a try, probably broke

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -49,6 +49,8 @@ int main(int argc, char **argv) {
 	std::string bootSelect = "/_nds/extras/bootSelect.nds";
 	std::string bootTouch = "/_nds/extras/bootTouch.nds";
 	std::string bootDefault = "/boot.nds";
+	std::string splash = "0"; //0 = off, 1 = on
+	//std::string splashlength = "1"; //1 = 1 second, up to 5 seconds
 	
 	videoSetMode(MODE_0_2D);
 	videoSetModeSub(MODE_0_2D);
@@ -75,6 +77,8 @@ int main(int argc, char **argv) {
 	bootSelect = ini.GetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
 	bootTouch = ini.GetString("BUTTONBOOT", "BOOT_TOUCH_PATH", bootTouch);
 	bootDefault = ini.GetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
+	splash = ini.GetString("BUTTONBOOT", "SPLASH", splash);
+	//splashlength = ini.GetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
 
 	ini.SetString("BUTTONBOOT", "BOOT_A_PATH", bootA);
 	ini.SetString("BUTTONBOOT", "BOOT_B_PATH", bootB);
@@ -89,11 +93,85 @@ int main(int argc, char **argv) {
 	ini.SetString("BUTTONBOOT", "BOOT_START_PATH", bootStart);
 	ini.SetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
 	ini.SetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
+	ini.SetString("BUTTONBOOT", "SPLASH", splash);
+	//ini.SetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
 
 	mkdir("/_nds/",0777);
 	mkdir("/_nds/extras/",0777);
 	ini.SaveIniFile("/_nds/extras/ButtonBoot.ini");
 
+void InitBMP() {
+
+	if (splash = 0) {
+		// Do nothing
+	} else {
+		videoSetMode(MODE_0_2D | DISPLAY_BG0_ACTIVE);
+		videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);
+		vramSetBankA (VRAM_A_MAIN_BG_0x06000000);
+		vramSetBankC (VRAM_C_SUB_BG_0x06200000);
+		REG_BG0CNT = BG_MAP_BASE(0) | BG_COLOR_256 | BG_TILE_BASE(2);
+		REG_BG0CNT_SUB = BG_MAP_BASE(0) | BG_COLOR_256 | BG_TILE_BASE(2);
+		BG_PALETTE[0]=0;
+		BG_PALETTE[255]=0xffff;
+		u16* bgMapTop = (u16*)SCREEN_BASE_BLOCK(0);
+		u16* bgMapSub = (u16*)SCREEN_BASE_BLOCK_SUB(0);
+		for (int i = 0; i < CONSOLE_SCREEN_WIDTH*CONSOLE_SCREEN_HEIGHT; i++) {
+			bgMapTop[i] = (u16)i;
+			bgMapSub[i] = (u16)i;
+		}
+	}
+
+}
+	
+void LoadBMP() {
+	FILE* file = fopen(("/_nds/extras/splash.bmp"), "rb");
+
+	fseek(file, 0xe, SEEK_SET);
+	u8 pixelStart = (u8)fgetc(file) + 0xe;
+	fseek(file, pixelStart, SEEK_SET);
+	for (int y=191; y>=0; y--) {
+		u16 buffer[256];
+		fread(buffer, 2, 0x100, file);
+		u16* src = buffer;
+		for (int i=0; i<256; i++) {
+			u16 val = *(src++);
+				BG_GFX[0x20000+y*256+i] = ((val>>10)&0x1f) | ((val)&(0x1f<<5)) | (val&0x1f)<<10 | BIT(15);
+			}
+		}
+	}
+
+	fclose(file);
+}
+	
+void LoadScreen() {
+	if (SplashFound) {
+		consoleInit(NULL, 0, BgType_Text4bpp, BgSize_T_256x256, 15, 0, true, true);
+		consoleClear();
+		consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 15, 0, false, true);
+		consoleClear();
+
+		if (SplashFound) {
+			// Set up background
+			videoSetMode(MODE_3_2D | DISPLAY_BG3_ACTIVE);
+			vramSetBankD(VRAM_D_MAIN_BG_0x06040000);
+			REG_BG3CNT = BG_MAP_BASE(16) | BG_BMP16_256x256;
+			REG_BG3X = 0;
+			REG_BG3Y = 0;
+			REG_BG3PA = 1<<8;
+			REG_BG3PB = 0;
+			REG_BG3PC = 0;
+			REG_BG3PD = 1<<8;
+
+			LoadBMP(true);
+		}
+	} else {
+		// Display Load Screen
+		swiDecompressLZSSVram ((void*)topLoadTiles, (void*)CHAR_BASE_BLOCK(2), 0, &decompressBiosCallback);
+		vramcpy_ui (&BG_PALETTE[0], topLoadPal, topLoadPalLen);
+	}
+}
+
+void Buttons() {
 
   scanKeys();
 	int pressed = keysHeld();
