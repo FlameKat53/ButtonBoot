f94ba7a36464cfbee923da90f9f20acca01c2b76
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-19 10:41:45 -0700

    move things that were in main back to there

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -36,71 +36,6 @@ void stop (void) {
 
 bool SplashFound = true;
 
-	std::string bootA = "/_nds/extras/bootA.nds";
-	std::string bootB = "/_nds/extras/bootB.nds";
-	std::string bootX = "/_nds/extras/bootX.nds";
-	std::string bootY = "/_nds/extras/bootY.nds";
-	std::string bootL = "/_nds/extras/bootL.nds";
-	std::string bootR = "/_nds/extras/bootR.nds";
-	std::string bootDown = "/_nds/extras/bootDown.nds";
-	std::string bootUp = "/_nds/extras/bootUp.nds";
-	std::string bootLeft = "/_nds/extras/bootLeft.nds";
-	std::string bootRight = "/_nds/extras/bootRight.nds";
-	std::string bootStart = "/_nds/extras/bootStart.nds";
-	std::string bootSelect = "/_nds/extras/bootSelect.nds";
-	std::string bootTouch = "/_nds/extras/bootTouch.nds";
-	std::string bootDefault = "/boot.nds";
-	std::string splash = "0"; //0 = off, 1 = on
-	//std::string splashlength = "1"; //1 = 1 second, up to 5 seconds
-	
-	videoSetMode(MODE_0_2D);
-	videoSetModeSub(MODE_0_2D);
-	vramSetBankH(VRAM_H_SUB_BG);
-	consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 15, 0, false, true);
-
-	if (!fatInitDefault()) {
-		iprintf ("fatInitDefault failed!\n");
-		stop();
-	}
-	CIniFile ini("/_nds/extras/ButtonBoot.ini");
-	
-	bootA = ini.GetString("BUTTONBOOT", "BOOT_A_PATH", bootA);
-	bootB = ini.GetString("BUTTONBOOT", "BOOT_B_PATH", bootB);
-	bootX = ini.GetString("BUTTONBOOT", "BOOT_X_PATH", bootX);
-	bootY = ini.GetString("BUTTONBOOT", "BOOT_Y_PATH", bootY);
-	bootL = ini.GetString("BUTTONBOOT", "BOOT_L_PATH", bootL);
-	bootR = ini.GetString("BUTTONBOOT", "BOOT_R_PATH", bootR);
-	bootDown = ini.GetString("BUTTONBOOT", "BOOT_DOWN_PATH", bootDown);
-	bootUp = ini.GetString("BUTTONBOOT", "BOOT_UP_PATH", bootUp);
-	bootLeft = ini.GetString("BUTTONBOOT", "BOOT_LEFT_PATH", bootLeft);
-	bootRight = ini.GetString("BUTTONBOOT", "BOOT_RIGHT_PATH", bootRight);
-	bootStart = ini.GetString("BUTTONBOOT", "BOOT_START_PATH", bootStart);
-	bootSelect = ini.GetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
-	bootTouch = ini.GetString("BUTTONBOOT", "BOOT_TOUCH_PATH", bootTouch);
-	bootDefault = ini.GetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
-	splash = ini.GetString("BUTTONBOOT", "SPLASH", splash);
-	//splashlength = ini.GetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
-
-	ini.SetString("BUTTONBOOT", "BOOT_A_PATH", bootA);
-	ini.SetString("BUTTONBOOT", "BOOT_B_PATH", bootB);
-	ini.SetString("BUTTONBOOT", "BOOT_X_PATH", bootX);
-	ini.SetString("BUTTONBOOT", "BOOT_Y_PATH", bootY);
-	ini.SetString("BUTTONBOOT", "BOOT_L_PATH", bootL);
-	ini.SetString("BUTTONBOOT", "BOOT_R_PATH", bootR);
-	ini.SetString("BUTTONBOOT", "BOOT_DOWN_PATH", bootDown);
-	ini.SetString("BUTTONBOOT", "BOOT_UP_PATH", bootUp);
-	ini.SetString("BUTTONBOOT", "BOOT_LEFT_PATH", bootLeft);
-	ini.SetString("BUTTONBOOT", "BOOT_RIGHT_PATH", bootRight);
-	ini.SetString("BUTTONBOOT", "BOOT_START_PATH", bootStart);
-	ini.SetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
-	ini.SetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
-	ini.SetString("BUTTONBOOT", "SPLASH", splash);
-	//ini.SetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
-
-	mkdir("/_nds/",0777);
-	mkdir("/_nds/extras/",0777);
-	ini.SaveIniFile("/_nds/extras/ButtonBoot.ini");
-
 void BootSplashInit() {
 
 	if (SplashFound) {
@@ -170,6 +105,70 @@ void LoadScreen() {
 
 int main(int argc, char **argv) {
 
+	std::string bootA = "/_nds/extras/bootA.nds";
+	std::string bootB = "/_nds/extras/bootB.nds";
+	std::string bootX = "/_nds/extras/bootX.nds";
+	std::string bootY = "/_nds/extras/bootY.nds";
+	std::string bootL = "/_nds/extras/bootL.nds";
+	std::string bootR = "/_nds/extras/bootR.nds";
+	std::string bootDown = "/_nds/extras/bootDown.nds";
+	std::string bootUp = "/_nds/extras/bootUp.nds";
+	std::string bootLeft = "/_nds/extras/bootLeft.nds";
+	std::string bootRight = "/_nds/extras/bootRight.nds";
+	std::string bootStart = "/_nds/extras/bootStart.nds";
+	std::string bootSelect = "/_nds/extras/bootSelect.nds";
+	std::string bootTouch = "/_nds/extras/bootTouch.nds";
+	std::string bootDefault = "/boot.nds";
+	std::string splash = "0"; //0 = off, 1 = on
+	//std::string splashlength = "1"; //1 = 1 second, up to 5 seconds
+	
+	videoSetMode(MODE_0_2D);
+	videoSetModeSub(MODE_0_2D);
+	vramSetBankH(VRAM_H_SUB_BG);
+	consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 15, 0, false, true);
+
+	if (!fatInitDefault()) {
+		iprintf ("fatInitDefault failed!\n");
+		stop();
+	}
+	CIniFile ini("/_nds/extras/ButtonBoot.ini");
+	
+	bootA = ini.GetString("BUTTONBOOT", "BOOT_A_PATH", bootA);
+	bootB = ini.GetString("BUTTONBOOT", "BOOT_B_PATH", bootB);
+	bootX = ini.GetString("BUTTONBOOT", "BOOT_X_PATH", bootX);
+	bootY = ini.GetString("BUTTONBOOT", "BOOT_Y_PATH", bootY);
+	bootL = ini.GetString("BUTTONBOOT", "BOOT_L_PATH", bootL);
+	bootR = ini.GetString("BUTTONBOOT", "BOOT_R_PATH", bootR);
+	bootDown = ini.GetString("BUTTONBOOT", "BOOT_DOWN_PATH", bootDown);
+	bootUp = ini.GetString("BUTTONBOOT", "BOOT_UP_PATH", bootUp);
+	bootLeft = ini.GetString("BUTTONBOOT", "BOOT_LEFT_PATH", bootLeft);
+	bootRight = ini.GetString("BUTTONBOOT", "BOOT_RIGHT_PATH", bootRight);
+	bootStart = ini.GetString("BUTTONBOOT", "BOOT_START_PATH", bootStart);
+	bootSelect = ini.GetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
+	bootTouch = ini.GetString("BUTTONBOOT", "BOOT_TOUCH_PATH", bootTouch);
+	bootDefault = ini.GetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
+	splash = ini.GetString("BUTTONBOOT", "SPLASH", splash);
+	//splashlength = ini.GetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
+
+	ini.SetString("BUTTONBOOT", "BOOT_A_PATH", bootA);
+	ini.SetString("BUTTONBOOT", "BOOT_B_PATH", bootB);
+	ini.SetString("BUTTONBOOT", "BOOT_X_PATH", bootX);
+	ini.SetString("BUTTONBOOT", "BOOT_Y_PATH", bootY);
+	ini.SetString("BUTTONBOOT", "BOOT_L_PATH", bootL);
+	ini.SetString("BUTTONBOOT", "BOOT_R_PATH", bootR);
+	ini.SetString("BUTTONBOOT", "BOOT_DOWN_PATH", bootDown);
+	ini.SetString("BUTTONBOOT", "BOOT_UP_PATH", bootUp);
+	ini.SetString("BUTTONBOOT", "BOOT_LEFT_PATH", bootLeft);
+	ini.SetString("BUTTONBOOT", "BOOT_RIGHT_PATH", bootRight);
+	ini.SetString("BUTTONBOOT", "BOOT_START_PATH", bootStart);
+	ini.SetString("BUTTONBOOT", "BOOT_SELECT_PATH", bootSelect);
+	ini.SetString("BUTTONBOOT", "BOOT_DEFAULT_PATH", bootDefault);
+	ini.SetString("BUTTONBOOT", "SPLASH", splash);
+	//ini.SetString("BUTTONBOOT", "SPLASH_LENGTH", splashlength);
+
+	mkdir("/_nds/",0777);
+	mkdir("/_nds/extras/",0777);
+	ini.SaveIniFile("/_nds/extras/ButtonBoot.ini");
 		if (splash) {
 
 			if (access("/_nds/extras/splash.bmp", F_OK)) SplashFound = false;
