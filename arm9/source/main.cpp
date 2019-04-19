a03050038fdbab9c4ee1b8ff45a3499f4de6b465
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-19 11:39:31 -0700

    Update main.cpp

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -149,7 +149,7 @@ int main(int argc, char **argv) {
 	mkdir("/_nds/extras/",0777);
 	ini.SaveIniFile("/_nds/extras/ButtonBoot.ini");
 
-		if (splash) {
+		if (splash == "1") {
 
 			if (access("/_nds/extras/splash.bmp", F_OK)) SplashFound = false;
 
