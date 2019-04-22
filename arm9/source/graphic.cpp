ba5836ca415197ef143982333b9e8ff388c66a11
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-22 09:26:17 -0700

    Update graphic.cpp

--- arm9/source/graphic.cpp
+++ arm9/source/graphic.cpp
@@ -1,4 +1,6 @@
-	/*// Display GM9i logo
+#include "graphic.h"
+
+/*	// Display GM9i logo
 	bg3 = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 1, 0);
 	bgSetScroll(bg3, 0, 0);
 	decompress(image_testBitmap, bgGetGfxPtr(bg3), LZ77Vram);
@@ -8,6 +10,38 @@
 		swiWaitForVBlank();
 	}
 */
+u16 bmpImageBuffer[256*192];
+u16 videoImageBuffer[39][256*144];
+
+u16 convertToDsBmp(u16 val) {
+	if (ms().colorMode == 1) {
+		u16 newVal = ((val>>10)&31) | (val&31<<5) | (val&31)<<10 | BIT(15);
+
+		u8 b,g,r,max,min;
+		b = ((newVal)>>10)&31;
+		g = ((newVal)>>5)&31;
+		r = (newVal)&31;
+		// Value decomposition of hsv
+		max = (b > g) ? b : g;
+		max = (max > r) ? max : r;
+
+		// Desaturate
+		min = (b < g) ? b : g;
+		min = (min < r) ? min : r;
+		max = (max + min) / 2;
+		
+		newVal = 32768|(max<<10)|(max<<5)|(max);
+
+		b = ((newVal)>>10)&(31-6*ms().blfLevel);
+		g = ((newVal)>>5)&(31-3*ms().blfLevel);
+		r = (newVal)&31;
+
+		return 32768|(b<<10)|(g<<5)|(r);
+	} else {
+		return ((val>>10)&31) | (val&(31-3*ms().blfLevel)<<5) | (val&(31-6*ms().blfLevel))<<10 | BIT(15);
+	}
+}
+
 void LoadBMP(void) {
 	dmaFillHalfWords(0, BG_GFX, 0x18000);
 
