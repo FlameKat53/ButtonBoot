fd96bea5102970b81beead2ffdb3bee7d2500e30
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-22 09:46:44 -0700

    whoops

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -24,7 +24,7 @@
 #include <fat.h>
 #include "common/nds_loader_arm9.h"
 #include "inifile.h"
-#include "graphic.h"
+//#include "graphic.h"
 #include "void.h"
 //---------------------------------------------------------------------------------
 void stop (void) {
